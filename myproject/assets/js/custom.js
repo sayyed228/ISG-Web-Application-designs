dc4304f834d69999a7e3774d1b7a3f91f1248e78
unknown <nsayyed@CTS-ANDROID-1.cogitate.india>
2015-09-15 20:40:19 +0530

    version v.1.3

--- myproject/assets/js/custom.js
+++ myproject/assets/js/custom.js
@@ -1,26 +1,38 @@
 "use stirct";
 
 $(document).ready(function() {
-	$('.ISG-Tabs li:first-child a').find('.active').show();
-/*
-    $('.ISG-Tabs li a').hover(
-        function() {
-            $(this).find('.normal').hide();
-            $(this).find('.active').show();
-        },
-        function() {
-            $(this).find('.active').hide();
-			$(this).find('.normal').show();
-        }
-    );*/
+    $('.ISG-Tabs li:first-child a').find('.active').show();
+
+
+
+    $('.box1').click(function() {
+        $('.submissionId').show();
+        $('.submissionId').addClass('swing animated');
+		$('.submissionId .panel-heading').text('Enter Submission ID');
+        swing();
+    });
 	
-	$('.ISG-Tabs li a').click(function() {
-		
-		 $('.ISG-Tabs li a').find('.active').hide();
-		 $('.ISG-Tabs li a').find('.normal').show();
-		 $(this).find('.normal').hide();
-		 $(this).find('.active').show();
-	});
+	$('.box2').click(function() {
+        $('.submissionId').show();
+        $('.submissionId').addClass('swing animated');
+		$('.submissionId .panel-heading').text('Policy Number');
+        swing();
+    });
+
+    function swing() {
+        setTimeout(function() {
+            $('.submissionId').removeClass('swing animated');
+        }, 2000);
+    }
+
+
+    $('.ISG-Tabs li a').click(function() {
+
+        $('.ISG-Tabs li a').find('.active').hide();
+        $('.ISG-Tabs li a').find('.normal').show();
+        $(this).find('.normal').hide();
+        $(this).find('.active').show();
+    });
 
 
 
