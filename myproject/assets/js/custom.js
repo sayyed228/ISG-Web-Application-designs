720a9b545d72393f0604b3233ffda8add17df7b5
unknown <nsayyed@CTS-ANDROID-1.cogitate.india>
2015-09-16 18:24:57 +0530

    version v.1.4

--- myproject/assets/js/custom.js
+++ myproject/assets/js/custom.js
@@ -8,14 +8,18 @@ $(document).ready(function() {
     $('.box1').click(function() {
         $('.submissionId').show();
         $('.submissionId').addClass('swing animated');
-		$('.submissionId .panel-heading').text('Enter Submission ID');
+		//$('.submissionId .panel-heading').text('Enter Submission ID');
+		$('.box2').removeClass('activebox');
+		$(this).addClass('activebox');
         swing();
     });
 	
 	$('.box2').click(function() {
         $('.submissionId').show();
         $('.submissionId').addClass('swing animated');
-		$('.submissionId .panel-heading').text('Policy Number');
+		//$('.submissionId .panel-heading').text('Policy Number');
+		$('.box1').removeClass('activebox');
+		$(this).addClass('activebox');
         swing();
     });
 
@@ -33,6 +37,56 @@ $(document).ready(function() {
         $(this).find('.normal').hide();
         $(this).find('.active').show();
     });
+
+
+
+
+$('.forgotpassword').click(function() {
+	$('.loginboxwrap').hide();
+	$('.resetwrap').show();
+	$('.resetwrap').addClass('flipInY animated');
+	
+	 
+	});
+	
+	$('.back-to-login').click(function() {
+	$('.resetwrap').hide();
+	$('.loginboxwrap').show();
+	$('.loginboxwrap').addClass('flipInY animated');
+	
+	 
+	});
+
+
+
+
+
+
+
+$('.btn-toggle').click(function() {
+    $(this).find('.btn').toggleClass('active');  
+    
+    if ($(this).find('.btn-primary').size()>0) {
+    	$(this).find('.btn').toggleClass('btn-primary');
+    }
+    if ($(this).find('.btn-danger').size()>0) {
+    	$(this).find('.btn').toggleClass('btn-danger');
+    }
+    if ($(this).find('.btn-success').size()>0) {
+    	$(this).find('.btn').toggleClass('btn-success');
+    }
+    if ($(this).find('.btn-info').size()>0) {
+    	$(this).find('.btn').toggleClass('btn-info');
+    }
+    
+    $(this).find('.btn').toggleClass('btn-default');
+       
+});
+
+$('form').submit(function(){
+	//alert($(this["options"]).val());
+    return false;
+});
 
 
 
